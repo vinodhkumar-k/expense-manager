3c5874463d0910ab018e40c568378e4b3293cbb4
vinodhkumarkonda <vinodhkumar.prs@gmail.com>
2020-09-11 23:02:05 +0530

    Modified Title and added header footer sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Toolbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
 import './App.css';
 
+const useStyles = makeStyles({
+  header: {
+    backgroundColor: '#5b616c',
+    minHeight: 45,
+    maxHeight: 45,
+    // font: 'Charlie Text',
+    color: 'white'
+  },
+  footer: {
+    bottom: 0,
+    minWidth: '100%',
+    color: 'white',
+    backgroundColor: '#343a40',
+    height: 45,
+    fontSize: 12,
+    margin: 0,
+    padding: 0,
+    lineHeight: '45px',
+    position: 'fixed'
+  }
+});
+
 function App() {
+  const styles = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Toolbar className={styles.header}>Expenses Manager</Toolbar>
       </header>
+      <footer className={styles.footer}>Footer goes here</footer>
     </div>
   );
 }
