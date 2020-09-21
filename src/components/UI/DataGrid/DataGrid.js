1e5a3f65c27ec2783d7ab40dad991706ceb5a6ff
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-09-21 23:06:17 +0530

    Update DataGrid.js

--- src/components/UI/DataGrid/DataGrid.js
+++ src/components/UI/DataGrid/DataGrid.js
@@ -23,7 +23,6 @@ const useStyles = makeStyles({
 });
 
 const DataGrid = (props) => {
-  
   // ToDo: Column Headers & row data should come as props
   const styles = useStyles();
   const columnHeaders = ['No.', 'DATE', 'CATEGORY', 'DETAILS', 'AMOUNT'];
