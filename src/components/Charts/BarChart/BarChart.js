284df70a47b4576edeae37af2bb0ceac7cfe2e53
vinodhkumarkonda <vinodhkumar.prs@gmail.com>
2020-09-23 23:16:43 +0530

    Bar chart changes

--- src/components/Charts/BarChart/BarChart.js
+++ src/components/Charts/BarChart/BarChart.js
@@ -6,9 +6,9 @@ import { useD3 } from '../../../hooks/useD3';
 const BarChart = ({data}) => {
   const ref = useD3(
     (svg) => {
-      const height = 500;
+      const height = 250;
       const width = 500;
-      const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+      const margin = { top: 20, right: 30, bottom: 0, left: 40 };
 
       const x = d3.scaleBand()
                   .domain(data.map(d => d.DATE))
@@ -16,19 +16,33 @@ const BarChart = ({data}) => {
                   .padding(0.1);
 
       const y = d3.scaleLinear()
-                  .domain([0, d3.max(data, d => d.sales)])
+                  .domain([0, d3.max(data, d => d.AMOUNT)])
                   .rangeRound([height - margin.bottom, margin.top]);
 
-      svg.append('g').call(d3.axisLeft(y));
+      /*svg.append('g').call(d3.axisLeft(y));
       
       svg.append('g')
-         .call(d3.axisBottom(x));
+         .attr('transform', `translate(0, ${height})`)
+         .call(d3.axisBottom(x))
+         .attr('x', x.bandwidth()/2)
+         .attr('y', 0);*/
+      const xAxis =  (g) => 
+                      g.call(d3.axisBottom(x))
+                       .attr('transform', `translate(0, ${height})`);
+            
+      const yAxis = (g) => 
+                     g.call(d3.axisLeft(y))
+                      .text(data.AMOUNT)
+      
+      svg.select(".x-axis").call(xAxis);
+      svg.select(".y-axis").call(yAxis);
+      
     },
     [data.length]
   );
 
   return (
-    <svg ref={ref} style={{height: 500, width: '100%', marginRight: 0, marginLeft: 0}}>
+    <svg ref={ref} style={{height: 350, width: '100%', marginRight: 0, marginLeft: 0}}>
       <g className="plot-area"/>
       <g className="x-axis"/>
       <g className="y-axis"/>
