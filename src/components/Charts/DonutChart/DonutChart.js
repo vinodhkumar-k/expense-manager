908636116a7bb3649276f89b2fd018c6cd60e507
vinodhkumarkonda <vinodhkumar.prs@gmail.com>
2020-09-29 18:33:23 +0530

    Added Donut Chart

---
+++ src/components/Charts/DonutChart/DonutChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+import { useD3 } from '../../../hooks/useD3';
+
+const DonutChart = () => {
+  return (
+    <svg></svg>
+  );
+}
+
+export default DonutChart;
