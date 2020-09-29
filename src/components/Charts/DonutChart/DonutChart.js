b9af62af880b5347c50105d26479bb68c0ade963
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-09-29 20:48:37 +0530

    Update DonutChart.js

--- src/components/Charts/DonutChart/DonutChart.js
+++ src/components/Charts/DonutChart/DonutChart.js
@@ -2,10 +2,13 @@ import React from 'react';
 import * as d3 from 'd3';
 
 import { useD3 } from '../../../hooks/useD3';
+import { schemeCategory10 } from 'd3';
 
 const DonutChart = () => {
   return (
-    <svg></svg>
+    <svg ref={ref} style={{ height: 300, width: 300, marginRight: 0, marginLeft: 0 }}>
+      <g className="plot-area" />
+    </svg>
   );
 }
 
