1c5fd911a69f3d487471df1b1688cb189de7e0d7
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-09-29 20:49:22 +0530

    Update DonutChart.js

--- src/components/Charts/DonutChart/DonutChart.js
+++ src/components/Charts/DonutChart/DonutChart.js
@@ -5,6 +5,18 @@ import { useD3 } from '../../../hooks/useD3';
 import { schemeCategory10 } from 'd3';
 
 const DonutChart = () => {
+  const ref = useD3(
+    (svg) => {
+      const height = 300;
+      const width = 300;
+      const totalRadius = Math.min(width, height) / 2;
+      const donutHoleRadius = totalRadius * 0.5;
+      const color = d3.scaleOrdinal(schemeCategory10);
+
+      const arc = d3.arc().innerRadius(totalRadius - donutHoleRadius).outerRadius(totalRadius);
+    },
+    [] // pass length property
+  );
   return (
     <svg ref={ref} style={{ height: 300, width: 300, marginRight: 0, marginLeft: 0 }}>
       <g className="plot-area" />
