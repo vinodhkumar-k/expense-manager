f8679c83918973dc46419cf5ef1213416b7d288d
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-04 09:50:58 +0530

    Update MonthlyInsights.js

--- src/containers/MonthlyInsights/MonthlyInsights.js
+++ src/containers/MonthlyInsights/MonthlyInsights.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Tiles from '../../components/UI/Tiles/Tiles';
 
-const MonthlyInsights = () => <h1 style={{ height: 45 }}>MonthlyInsights</h1>;
+// const MonthlyInsights = () => <h1 style={{ height: 45 }}>MonthlyInsights</h1>;
+const MonthlyInsights = () => {
+  return (<Tiles/>)
+};
 
 export default MonthlyInsights;
