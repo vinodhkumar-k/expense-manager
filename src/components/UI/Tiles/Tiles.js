e06c76270b271cf7757380dd1934a08bb80d8b20
vinodhkumarkonda <vinodhkumar.prs@gmail.com>
2020-10-04 09:39:35 +0530

    Added Tiles Page

---
+++ src/components/UI/Tiles/Tiles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June']; // Move this to helpers file
+
+const useStyles = makeStyles({
+  tile: {
+  }
+})
+
+const Tiles = () => {
+
+};
+
+export default Tiles;
