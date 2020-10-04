a4cb17e55267cbca137f0eac7d6f5642ad618ae7
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-04 09:51:43 +0530

    Update Tiles.js

--- src/components/UI/Tiles/Tiles.js
+++ src/components/UI/Tiles/Tiles.js
@@ -5,11 +5,34 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June']; // Move
 
 const useStyles = makeStyles({
   tile: {
+    width: '10em',
+    height: '10em',
+    background: '#00BCD4',
+    margin: 5,
+    float: 'left',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //fontFamily: Roboto, sans-serif
+    color: '#FFF',
+    '&:hover': {
+      background: '#0d6672',
+      cursor: 'pointer'
+    }
   }
 })
 
 const Tiles = () => {
-
+  const styles = useStyles();
+  return (
+    <div>
+      {
+        months.map((item, index) => (
+          <div key={item}>{item}</div>
+        ))
+      }
+    </div>
+  );
 };
 
 export default Tiles;
