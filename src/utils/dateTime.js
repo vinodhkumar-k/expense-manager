f3c0c8479577d4d3dad316029856522364a99c85
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-06 22:46:27 +0530

    Update dateTime.js

---
+++ src/utils/dateTime.js
@@ -0,0 +1,42 @@
+import moment from 'moment';
+import _ from 'lodash';
+
+const dateTime = (() => {
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  const getCurrentMonth = () => {
+    return months[moment().month()];
+  };
+
+  const getCurrentTimestamp = () => {
+    return new Date().getTime();
+  };
+
+  const formatDate = (date) => {
+    return moment(date).format('DD-MM-YYYY');
+  };
+
+  const sortByMonth = (data) => {
+    const sortedDataByMonth = [];
+    _.forEach(months, (month) => {
+      const dataForTheMonth = _.find(data, {_id: month.toLowerCase()});
+      if(dataForTheMonth) {
+        sortedDataByMonth.push(dataForTheMonth);
+      }
+    });
+    return sortedDataByMonth;
+  };
+
+  return {
+    months,
+    getCurrentMonth,
+    getCurrentTimestamp,
+    formatDate,
+    sortByMonth
+  };
+})();
+
+export default dateTime;
