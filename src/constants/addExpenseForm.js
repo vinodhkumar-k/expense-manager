183cf2657d8521c9c970b867a92eda3fcd584b82
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-13 20:14:24 +0530

    Update addExpenseForm.js

---
+++ src/constants/addExpenseForm.js
@@ -0,0 +1,15 @@
+export const CATEGORIES = [
+  'Home & Utility',
+  'Transport',
+  'Food & Drinks',
+  'Health & Personal Care',
+  'Clothing & Footwear',
+  'Education',
+  'Gifts',
+  'Celebrations',
+  'Charity',
+  'Entertainment',
+  'Loans',
+  'Sports',
+  'Others',
+];
