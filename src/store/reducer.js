ff02d7dd2f79edc46d142a499bd9b7f532cf2b61
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-29 10:01:55 +0530

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,24 @@
+import * as actionTypes from './actionTypes';
+
 const initialState = {
   monthlyExpenseDetails: [],
   categoryWiseExpenseForAMonth: []
 }
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_EXPENSE:
+      return Object.assign({}, state, {
+        monthlyExpenseDetails: [...state.monthlyExpenseDetails, action.payload]
+      });
+  
+    case actionTypes.SET_MONTHLY_EXPENSE_DETAILS:
+      return Object.assign({}, state, {
+        monthlyExpenseDetails: action.payload
+      });
+    default:
+      return state;
+  }
+};
 
 export default reducer;
