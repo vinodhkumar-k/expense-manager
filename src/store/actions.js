e610132127b30dd44e5818bf5afd063213aa5eab
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2020-10-29 10:01:15 +0530

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,28 @@
 import axios from 'axios';
+import { dispatch } from 'd3';
 
 import config from '../configs/config.json';
+import * as actionTypes from './actionTypes';
 
 const API_BASE_URL = config.API_BASE_URL;
 
 export const addExpenseDetails = (expenseDetails) => {
-  console.log('addExpenseDetails');
-  return dipatch => {
+  return dispatch => {
     axios.post(`${API_BASE_URL}/expenses`, expenseDetails)
       .then(response => {
-        console.log(response);
+        if (response.data.ok === 1) {
+          dispatch({type: actionTypes.ADD_EXPENSE, payload: expenseDetails});
+        }
       })
       .catch(err => console.error(err));
+  };
+};
+
+export const fetchMonthlyExpenseDetails = (userId, month) => {
+  return dispatch => {
+    axios.post(`${API_BASE_URL}/expenses/${userId}/${month}`)
+      .then(response => {
+        console.log(response);
+      });
   };
 };
